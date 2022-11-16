f49c728c82c69b27a0f1198299baca4428631046
Richard Blaauw <richard.blaauw@ogd.nl>
2022-11-16 13:32:53 +0100

    Update combat section

--- js/data_action.js
+++ js/data_action.js
@@ -1,63 +1,64 @@
 data_action = [
     {
-        title: "Attack",
+        title: "Order of Attack",
         icon: "crossed-swords",
-        subtitle: "Melee or ranged attack",
-        description: "Perform a melee or ranged attack with your weapon",
-        reference: "PHB, pgs. 192,194-195.",
+        subtitle: "Who goes first",
+        description: "Who goes first",
+        reference: "add page number",
         bullets: [
-            "Certain features, such as the <i>Extra Attack</i> feature of the fighter, allow you to make more than one attack with this action. Each of these attacks is a separate roll and may target different creatures. You may move in between these attacks.",
-            "When you attack with a light melee weapon, you can use a bonus action to attack with your other hand (see the <i>Offhand attack</i> bonus action).",
-            "You may replace one of your melee attacks with a <i>Grapple</i> or a <i>Shove</i>.",
-            "Some conditions give advantage on the attack: attacks against blinded, paralyzed, petrified, restrained, stunned, or unconscious targets; melee attacks against prone targets; attacks by invisible or hidden attackers.",
-            "Some conditions give disadvantage on the attack: attacks against invisible or hidden targets; ranged attacks against prone targets; attacks by blinded, frightened, poisoned, or restrained attackers."
+            "Rank all characters in DEX order—highest goes first."
         ]
     },
     {
-        title: "Grapple",
+        title: "Surprise attacks",
         icon: "grab",
-        subtitle: "Special melee attack",
-        description: "Attempt to grab a creature or wrestle with it",
-        reference: "PHB, pg. 195.",
+        subtitle: "Getting the drop on someone",
+        description: "Getting the drop on someone",
+        reference: "add page number",
         bullets: [
-            "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
-            "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
-            "Using at least one free hand, you try to seize the target by making a grapple check, a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
-            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
+            "Allow a skill roll to determine whether the target anticipates the attack (Spot Hidden, Listen, Psychology).",
+            "If successful, switch to the usual DEX combat order.",
+            "Otherwise the attack automatically hits or gains a bonus die."             
         ]
     },
     {
-        title: "Shove",
+        title: "Actions",
         icon: "hand",
-        subtitle: "Special melee attack",
-        description: "Shove a creature, either to knock it prone or push it away from you",
-        reference: "PHB, pg. 195.",
+        subtitle: "1 action/round",
+        description: "Each character gets one action",
+        reference: "add page number",
         bullets: [
-            "Using the <i>Attack</i> action, you can make a special melee attack to shove a creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
-            "The target of your shove must be no more than one size larger than you, and it must be within your reach.",
-            "You make a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
-            "If you win the contest, you either knock the target prone or push it 5 feet away from you."
+            "Any action a character can make in a combat round—attacking, dodging, fighting back, fleeing, casting a spell, etc.",
+            "Note: some NPCs and monsters have multiple actions."
         ]
     },
     {
-        title: "Cast a spell",
+        title: "Attempt to harm",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 action",
-        description: "Cast a spell with a casting time of 1 action",
-        reference: "PHB, pg. 192.",
+        subtitle: "Intention to inflict harm",
+        description: "The character intents to inflict harm",
+        reference: "add page number",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-            "The target of a spell must be within the spell's range. To target something, you must have a clear path to it, so it can't be behind total cover.",
-            "Spells with material components do not consume the material unless explicitly stated. Unless the cost of a material is given, you can assume that the cost is negligible and the material is simply available in a component pouch.",
-            "Some spells require you to maintain concentration in order to keep their magic active. If you lose concentration, such a spell ends. You lose concentration on a spell if you cast another spell that requires concentration or when you are incapacitated. Each time you take damage, you must make a Constitution saving throw to maintain your concentration. The DC equals 10 or half the damage you take, whichever number is higher."
+            "The target of an attack may fight back, dodge the attack or attempt a maneuver.",
+            "Both sides make a skill roll.",
+            "If the character chooses to fight back, use the target’s Fighting skill to oppose the attacker’s Fighting skill",
+            "If the character chooses to dodge, use the target’s Dodge skill to oppose the attacker’s Fighting skill.",
+            " Determine who was successful.",
+            "Attacking a target that is fighting back: The side with the better level of success avoids being harmed, and inflicts damage on the opponent. In a draw the character initiating the attack beats the character that is fighting back. If both fail, no damage is inflicted.",
+            "Attacking a target that is dodging: If the attacker gains a higher level of success, the attack is successful and the attacker inflicts damage on the loser. Otherwise the attack is dodged and no damage is dealt. In a draw the character dodging wins and evades the attack. If both fail, no damage is inflicted.",
+            "Combat rolls cannot be pushed.",
+            "Roll damage for successful attacks (see Table XVII: Weapons page @@, or 1D3 for unarmed human attack)",
+            "If the attacker gains an Extreme level of success (not if fighting back) greater damage is inflicted:",
+            "If the attacker is using an impaling weapon: maximum damage (maximum damage plus maximum damage bonus) plus roll weapon damage. ",
+            "If the attacker is using a non-impaling weapon (e.g. blunt): maximum damage (maximum damage plus maximum damage bonus)."
         ]
     },
     {
-        title: "Dash",
+        title: "Fighting maneuvers",
         icon: "sprint",
-        subtitle: "Double movement speed",
-        description: "Gain extra movement for the current turn",
-        reference: "PHB, pg. 192.",
+        subtitle: "Disarm, knock prone, etc.",
+        description: "Goal is something other than inflicting physical harm ",
+        reference: "add page number",
         bullets: [
             "The increase equals your speed, after applying any modifiers."
         ]
@@ -67,7 +68,7 @@ data_action = [
         icon: "journey",
         subtitle: "Prevent opportunity attacks",
         description: "Your movement doesn't provoke opportunity attacks for the rest of the turn",
-        reference: "PHB, pg. 192.",
+        reference: "add page number",
         bullets: [
         ]
     },
@@ -76,7 +77,7 @@ data_action = [
         icon: "aura",
         subtitle: "Increase defenses",
         description: "Focus entirely on avoiding attacks",
-        reference: "PHB, pg. 192.",
+        reference: "add page number",
         bullets: [
             "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
             "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
@@ -87,7 +88,7 @@ data_action = [
         icon: "manacles",
         subtitle: "Escape a grapple",
         description: "Escape a grapple",
-        reference: "PHB, pg. 195.",
+        reference: "add page number",
         bullets: [
             "To escape a grapple, you must succeed on a Strength (Athletics) or Dexterity (Acrobatics) check contested by the grappler's Strength (Athletics) check.",
             "Escaping other conditions that restrain you (such as manacles) may require a Dexterity or Strength check, as specified by the condition."
@@ -98,7 +99,7 @@ data_action = [
         icon: "telepathy",
         subtitle: "Grant an ally advantage",
         description: "Grant an ally advantage on an ability check or attack",
-        reference: "PHB, pg. 192.",
+        reference: "add page number",
         bullets: [
             "The target gains advantage on the next ability check it makes to perform the task you are helping with.",
             "Alternatively, the target gains advantage on the next attack roll against against a creature within 5 feet of you.",
@@ -110,7 +111,7 @@ data_action = [
         icon: "snatch",
         subtitle: "Interact, use special abilities",
         description: "Interact with a second object or use special object abilities",
-        reference: "PHB, pg. 193.",
+        reference: "add page number",
         bullets: [
             "You can interact with one object for free during your turn (such as drawing a weapon or opening a door). If you want to interact with a second object, use this action.",
             "When an object requires your action for its use, you also take this action."
@@ -121,7 +122,7 @@ data_action = [
         icon: "round-shield",
         subtitle: "Equip or unequip a shield",
         description: "Equip or unequip a shield",
-        reference: "PHB, pgs. 144-146.",
+        reference: "add page number",
         bullets: [
             "A shield always takes an action to equip or unequip.",
             "Armor takes several minutes to equip or unequip."
@@ -132,7 +133,7 @@ data_action = [
         icon: "hood",
         subtitle: "",
         description: "Attempt to hide",
-        reference: "PHB, pg. 192.",
+        reference: "add page number",
         bullets: [
             "You can't hide from a creature that can see you. You must have total cover, be in a heavily obscured area, be invisible, or otherwise block the enemy's vision.",
             "If you make noise (such as shouting a warning or knocking over a vase), you give away your position.",
@@ -146,7 +147,7 @@ data_action = [
         icon: "magnifying-glass",
         subtitle: "",
         description: "Devote your attention to finding something",
-        reference: "PHB, pg. 193.",
+        reference: "add page number",
         bullets: [
             "Depending on the nature of your search, the DM might have you make a Wisdom (Perception) check or an Intelligence (Investigation) check."
         ]
@@ -156,7 +157,7 @@ data_action = [
         icon: "stopwatch",
         subtitle: "Choose trigger and action",
         description: "Choose a trigger and a response reaction",
-        reference: "PHB, pg. 193.",
+        reference: "add page number",
         bullets: [
             "First, you decide what perceivable circumstance will trigger your reaction.",
             "Then, you choose the action you will take in response to that trigger, or you choose to move up to your speed in response to it.",
@@ -169,7 +170,7 @@ data_action = [
         icon: "embrassed-energy",
         subtitle: "Some features use actions",
         description: "Use a racial or class feature that uses an action",
-        reference: "See class page for more information.",
+        reference: "add page number",
         bullets: [
 
         ]
@@ -179,7 +180,7 @@ data_action = [
         icon: "first-aid",
         subtitle: "Administer first aid to a dying creature",
         description: "Stop a dying creature from needing to make death saving throws",
-        reference: "PHB, pg. 197.",
+        reference: "add page number",
         bullets: [
             "Make a Wisdom (Medicine) check with DC 10",
             "On a success, the creature is stable and no longer needs to make death saving throws",
@@ -191,7 +192,7 @@ data_action = [
         icon: "juggler",
         subtitle: "Any action not on this list",
         description: "Perform any action you can imagine",
-        reference: "PHB, pg. 193.",
+        reference: "add page number",
         bullets: [
             "When you describe an action not detailed elsewhere in the rules, the DM tells you whether that action is possible and what kind of roll you need to make, if any, to determine success or failure."
         ]
